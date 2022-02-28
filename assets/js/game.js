206a7ef8f2f539d51ecff6b7312bbf904acf6e02
Hec98 <hector982015@gmail.com>
2022-02-28 00:32:13 -0600

    Function that returns a card from the deck

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,6 +9,7 @@ let deck = [];
 const types = ['C', 'D', 'H', 'S']
 const specials = ['A', 'J', 'Q', 'K']
 
+// This function creates a new deck
 const createDeck = () => {
   for(let i = 2; i<= 10; i++) {
     for(let type of types) {
@@ -20,10 +21,24 @@ const createDeck = () => {
       deck.push(spe + type)
     }
   }
-  console.log(deck);
+  // console.log(deck);
   deck = _.shuffle(deck);
   console.log(deck);
   return deck;
 };
 
 createDeck();
+
+// This function allows you to take a new card
+const takeCard = () => {
+  if (deck.length === 0) throw 'There are no cards in the deck';
+
+  card = deck.shift()
+  console.log(card);
+  console.log(deck);
+
+  return card;
+};
+
+// while(deck.length != 0) takeCard();
+// takeCard();
